d96cc9d2fb14ce85da4c3d2db0dab41da200f8d7
Yoo Hyun Sik <dbgustlr92@naver.com>
2017-11-08 16:58:36 +0900

    modify typo and comment

--- APKCrawler/Crawler.py
+++ APKCrawler/Crawler.py
@@ -5,35 +5,53 @@ import subprocess
 import re
 import os
 import time
-from DBController import DBController
 import sys
 from pyvirtualdisplay import Display
 import configparser
 import requests
 import logging
+from DBController import DBController
 
 class Crawler:
 
     def __init__(self, is_desktop):
+        """
+        생성자
+        is_desktop : 서버환경에서 실행시키는지, 데크스탑환경(GUI)에서 실행시키는지\
+                     (true, false)
+        """
+        # config.ini파일의 변수 가져오기
         config = configparser.ConfigParser()
         config.read('config.ini')
         self.apk_directory = config.get('Setting','APK_DIRECTORY')
         self.is_desktop = is_desktop
+
+        # 서버모드로 실행시켰다면 가상디스플레이 실행
         if(not is_desktop):
             self.display = Display(visible=0, size=(800, 600))
             self.display.start()
 
+        # 크롬 드라이버 실행 
         self.chrome = webdriver.Chrome(config.get('Setting',\
             'CHROME_DRIVER_DIRECTORY'))
+
+        # 크롤링할 디렉토리 리스트 저장
         self.category_list = config.items('PlayStoreURL')
+
+        # 데이터를 저장하고 제어할 DBController객체 생성
         self.db_connector = DBController(config.get('Setting','DB_DIRECTORY'))
+
+        # 메타데이터가 저장될 SQLite 테이블 생성
         self.db_connector.create_table()
 
     def __get_new_app_list(self, popular_url):
         """
-        카테고리별 인기차트를 Selenium 를 사용해 300개 앱의
-        메타정보를 가지고 온다.
+        (private)
+        입력받은 인기차트 url의 상위 300개 앱 메타데이터 수집
+        popular_url : 특정 카테고리의 인기차트 URL
         """
+
+        # 크롬 드라이버 url 이동 및 완료 대기
         self.chrome.get(popular_url)
         self.chrome.implicitly_wait(10)
 
@@ -47,6 +65,8 @@ class Crawler:
         # selector를 사용해 300개의 앱 div를 가져옴
         div_app_list =  self.chrome.find_elements_by_css_selector(\
             ".card.no-rationale.square-cover.apps.small")
+
+        # 300개의 div태그를 반복하면서 패키지 이름을 추출하여 리스트에 저장
         for div_app in div_app_list:
             app_detail = div_app.find_element_by_class_name('details')
             url = app_detail.find_element_by_class_name('title')\
@@ -58,8 +78,9 @@ class Crawler:
 
     def __get_app_detail(self, package_list):
         """
-        패키지 리스트를 입력으로 받아 앱별로 이름, 이미지소스,\
-        업데이트날짜를 크롤링함
+        (priavte)
+        패키지 리스트를 입력으로 받아 해당 패키지의 앱 이름, 이미지소스,\
+        업데이트날짜, 별점 개수 (ratings)를 크롤링함
         """
 
         # 앱 상세정보 페이지에 들어가기위한 기본url
@@ -70,18 +91,27 @@ class Crawler:
         for package in package_list:
             app_url = base_url + package
 
+            # 크롬 드라이버 페이지 이동 및 완료 대기
             self.chrome.get(app_url)
             self.chrome.implicitly_wait(10)
 
+            # 앱 이름, 이미지 소스, 최근 업데이트 날짜, 별점을 조회
+            # 원인은 파악하지 못했지만 exception발생하는 구간 존재
+            # TODO: Exception이 발생하는 구간을 정확하게 파악하여 예외처리
             try:
                 name = self.chrome.\
                     find_element_by_css_selector('.id-app-title').text
+
                 img_src = self.chrome.\
                     find_element_by_css_selector('.cover-image').\
                         get_attribute('src')
+
                 updated_date = self.chrome.\
                     find_elements_by_css_selector('.content')[0].text
-                ratings = self.chrome.find_elements_by_css_selector('.rating-count')[0].text
+
+                ratings = self.chrome.\
+                    find_elements_by_css_selector('.rating-count')[0].text
+                # 숫자 3자리마다 쉼표가 들어가므로 제거
                 if ',' in ratings:
                     ratings = ratings.replace(',','')
             except:
@@ -89,7 +119,7 @@ class Crawler:
                 print(package + " name, img_src, update_date 가져오기 실패")
                 continue
 
-            # 마지막에 None은 isDownloaded 컬럼에 해당된다.
+            # [앱 이름, 패키지 이름, 이미지 소스, 최신업데이트 날짜, APK다운 여부]
             detail_list.append([name, package, img_src, updated_date, False])
 
         return detail_list
@@ -97,13 +127,19 @@ class Crawler:
 
     def __download_apk(self,package_name,download_url):
         """
-        APK파일을 HTTP request를 통해 다운받는 함수
+        (private)
+        HTTP request를 통해 APK파일을 다운받음
         리퀘스트를 보내는 도중 에러가 발생하면 False반환
         정상적으로 파일이 저장완료되면 True반환
+        package_name : 다운받으려는 패키지 이름
+        download_url : HTTP request를 날리는 url 이름
         """
         file_name = str(package_name) + '.apk'
+
+        # timout 1분으로 설정하여 반응이 없는 것들은 예외처리
         try:
             r = requests.get(download_url, timeout=60)
+            # apk directory에 패키지이름.apk 형태로 저장
             with open(self.apk_directory + file_name,'wb') as apk:
                 apk.write(r.content)
         except requests.exceptions.Timeout as e:
@@ -116,20 +152,25 @@ class Crawler:
 
 
     def crawl_new(self):
-        # 카레고리별 플레이스토어 인기차트 긁어오기
+        """
+        (public)
+        카테고리 별 플레이스토어 인기차트 크롤링 및 DB 저장
+        """
         for category in self.category_list:
             category_name = category[0]
             url = category[1]
 
-            # Google Play Store를 크롤링하여 최신300개의 앱 메타정보를 가져오기
+            # 하나의 카테고리 인기차트에서 300개의 앱 패키지 이름 가져오기
             new_package_list = self.__get_new_app_list(url)
 
-            # 최신앱 메타정보로 갱신한 리스트를 입력으로 주고 
-            # 앱별로 상세정보를 크롤링함
-            # 이름, 업데이트날짜, 이미지소스
+            # 카테고리의 300개 앱 패키지 이름으로 300개 앱 상세정보 수집
+            # (앱 이름, 최신 업데이트 날짜, 이미지 소스, 레이팅) 
             updated_app_list = self.__get_app_detail(new_package_list)
 
-            # 새로 생긴된 데이터들을 DB에 업데이트
+            # 300개의 앱 메타 데이터를 DB에 업데이트
+            # 동일한 앱이 존재한다면 그대로 유지
+            # 하지만 동일한 앱에도 업데이트가 존재한다면 메타정보 업데이트
+            # 앱 이름이 DB에 없다면 새로 추가
             self.db_connector.update_app(updated_app_list, category_name)
 
         self.db_connector.commit_n_close()
@@ -137,6 +178,10 @@ class Crawler:
 
 
     def crawl_old(self):
+        """
+        (public)
+        기존 DB에 저장된 앱 메타데이터를 최신으로 업데이트
+        """
         for category in self.category_list:
             category_name = category[0]
             url = category[1]
@@ -145,9 +190,7 @@ class Crawler:
             old_package_list = self.db_connector\
                 .get_old_category_app_list(category)
 
-            # 최신앱 메타정보로 갱신한 리스트를 입력으로 주고 
-            # 앱별로 상세정보를 크롤링함
-            # 이름, 업데이트날짜, 이미지소스
+            # 기존 DB 메타데이터의 상세정보를 플레이스토어에서 크롤링
             updated_app_list = self.__get_app_detail(old_package_list)
 
             # 새로 생긴된 데이터들을 DB에 업데이트
@@ -155,24 +198,31 @@ class Crawler:
         self.db_connector.commit_n_close()
 
     def update_apk(self):
+        """
+        DB에서 다운받지 않은 APK파일을 찾아 APK파일을 다운로드
+        """
+
+        # DB에서 아직 다운받지 않은 APK파일의 리스트를 가져옴
         not_updated_list = self.db_connector.not_updated_list()
-        print(1)
+
         for package_row in not_updated_list:
             package_name = package_row[0]
+            # apkpure.com에 패키지 이름으로 검색
             search_url = 'http://apkpure.com/search?q=' + package_name
             self.chrome.get(search_url)
             self.chrome.implicitly_wait(10)
 
-            # 패키지명으로 검색하여 일치하는 앱 찾기
+            # 일치하는 앱이 검색되었는지 확인
             search_titles = self.chrome.\
                 find_elements_by_class_name('search-title')
 
-            # APK pure사이트에서 검색이 되지 않는 APK는 통과
+            # 검색결과가 없으면 apk를 다운받을 수 없으므로 통과
             if len(search_titles) == 0:
                 logging.info(package_name + " is not searched")
                 continue
 
-            # 검색결과와 일치하는 앱은 href링크에 패키지 이름이 들어있음
+            # 검색결과와 일치하는 앱의 href 속성에서 다운로드 링크 추출
+            # 검색결과가 여러개일 경우가 있으므로 패키지 이름으로 다시 확인
             link = ''
             for title in search_titles:
                 link = title.find_element_by_tag_name('a')
@@ -181,12 +231,12 @@ class Crawler:
                 if package_name in link:
                     break
 
-            # 검색결과가 여러개 나오지만 일치하지 않는다면 통과
+            # 검색결과가 여러개 나오지만 패키지명이 일치하지 않는다면 통과
             if link =='':
                 logging.info(package_name + ' is not searched in APKpure')
                 continue
 
-            print(link) # debug
+            # apk download링크로 이동
             self.chrome.get(link)
             self.chrome.implicitly_wait(10)
 
@@ -208,6 +258,8 @@ class Crawler:
                 logging.info(package_name + " does not have href or iframe")
                 continue
 
+
+            # apk 파일 다운로드가 성공하면 db에 True로 저장, 실패시 False로 저장
             if(self.__download_apk(package_name, src)):
                 self.db_connector.update_isdownload(package_name, True)
             else:
--- APKCrawler/DBController.py
+++ APKCrawler/DBController.py
@@ -10,16 +10,17 @@ class DBController:
     - commit_n_close : 변경내용 저장 및 종료
     - create_table : 앱 메타정보 저장하는 테이블 생성
     - get_old_category_app_list : 기존 DB에 존재하는 앱정보 불러오기
-    - get_all_app_name_list : 기존 DB에 존재하는 모든 앱이름만
+    - get_all_app_name_list : 기존 DB에 존재하는 모든 앱이름만\
         리스트로 가져오기
     - update_date : 메타정보를 입력받아 DB date컬럼 업데이트
-    - update_app : 해당 앱에 대하여 최신정보로 DB업데이트
-        (날짜 및 isDownloaded)
+    - update_app : 해당 앱에 대하여 최신정보로 DB업데이트\
+        (날짜 및 is_downloaded)
     - insert_app : 새로운 앱 메타정보를 테이블에 추가
     """
 
     def __init__(self, db_directory):
         """
+        (public)
         생성자. 새로운 db를 만들고 커서 생성
         """
         self.connection = sqlite3.connect(db_directory)
@@ -27,6 +28,7 @@ class DBController:
 
     def commit_n_close(self):
         """
+        (public)
         현재까지 변경된 내용을 저장한뒤, 종료
         """
         self.connection.commit()
@@ -34,6 +36,7 @@ class DBController:
 
     def create_table(self):
         """
+        (public)
         어플리케이션 메타정보를 저장하는 테이블 생성
         정상적으로 생성되거나, 이미 존재한다면 True반환.
         예외가 발생하면 False 반환
@@ -41,16 +44,16 @@ class DBController:
         try:
             self.cursor.execute("""
                 CREATE TABLE list(
-                    appName,
+                    app_name,
                     package,
-                    imgSrc,
-                    updateDate,
+                    img_src,
+                    update_date,
                     ratings,
-                    isDownloaded,
+                    is_downloaded,
                     category
                 )""")
 
-        # 테이블이 존재한다면 여기로 들어옴
+        # 테이블이 존재한다면 OperationalError 발생
         except sqlite3.OperationalError as e:
             print(e)
             return True
@@ -60,8 +63,10 @@ class DBController:
 
     def get_old_category_app_list(self,category):
         """
-        카테고리를 입력으로 받아 해당 카테고리에 속하고
-        기존 디비에 존재하는 패키지 이름들을 리스트로 반환
+        (public)
+        기존 DB에 존재하는 앱 메타 데이터중 입력으로 받은 카테고리에 속하는 앱\
+        리스트를 반환
+        category : 카테고리 이름
         """
         try:
             self.cursor.execute('SELECT package FROM list WHERE category==(?)'\
@@ -75,6 +80,7 @@ class DBController:
 
     def get_all_app_name_list(self):
         """
+        (public)
         DB에 존재하는 모든 앱이름 리스트로 반환
         """
         try:
@@ -86,18 +92,18 @@ class DBController:
             self.connection.close()
             raise e
 
-    def update_date(self, app):
+    def update_date(self, package_name, update_date):
         """
         앱 메타정보를 입력으로 받아 기존DB에 존재하던 앱정보를
-        최신정보로 업데이트 시킴. isDownloaded필드를 변경
+        최신정보로 업데이트 시킴. is_downloaded필드를 변경
         """
         try:
             self.cursor.execute(\
-                "update list set updateDate=? where appName=?",\
-                (app.update_date, app.name))
+                "update list set update_date=? where package=?",\
+                (update_date, package_name))
             self.cursor.execute(\
-                "update list set isDownloaded=? where appName=?",\
-                (None, app.name))
+                "update list set is_downloaded=? where package=?",\
+                (False, package_name))
             self.connection.commit()
         except Exception as e:
             print('update_date error')
@@ -106,8 +112,9 @@ class DBController:
 
     def update_app(self, update_app_list, category):
         """
-        입력 : 최신버전의 메타정보리스트
-        역할 : 입력정보를 DB에 반영
+        업데이트 혹은 추가된 앱 데이터들을 DB에 저장
+        update_app_list : 업데이트가 존재하는 앱 상세정보
+        category : 카테고리 이름
         """
 
         all_app_list = self.get_all_app_name_list()
@@ -115,23 +122,23 @@ class DBController:
         for new_app in update_app_list:
             new_app_name = new_app[0]
             package = new_app[1]
-            imgSrc = new_app[2]
-            updateDate = new_app[3]
+            img_src = new_app[2]
+            update_date = new_app[3]
             ratings = new_app[4]
-            isDownloaded = new_app[5]
+            is_downloaded = new_app[5]
 
             # 기존 DB에 존재하던 앱이라면 업데이트날짜를 비교해서 
             # 동일하면 그대로
             # 업데이트가 날짜가 다르다면(업데이트가 존재한다면)
-            # 업데이트 날짜 수정 및 isDownloaded 컬럼 수정
+            # 업데이트 날짜 수정 및 is_downloaded 컬럼 수정
             self.cursor.execute(\
-                "select * from list where appName=(?) LIMIT 1",\
+                "select * from list where app_name=(?) LIMIT 1",\
                 (new_app_name,))
 
             # 기존 DB에 없던 앱이라면 새로 DB에 추가시힘
             app_details = self.cursor.fetchone()[0]
             if data == 0:
-                self.insert_app(new_app_name,package,imgSrc,updateDate,ratings,isDownloaded,category)
+                self.insert_app(new_app_name,package,img_src,update_date,ratings,is_downloaded,category)
                 logging.info(new_app_name + ' is inserted')
                 continue
 
@@ -144,17 +151,20 @@ class DBController:
                 logging.info(new_app_name + ' is already updated')
                 continue
             else:
-                self.update_date(app)
+                self.update_date(package, update_date)
                 logging.info(new_app_name + ' is updated')
 
 
-    def insert_app(self,new_app_name,package,imgSrc,updateDate,ratings,isDownloaded,category):
+    def insert_app(self, new_app_name, package, img_src, update_date,\
+                    ratings, is_downloaded, category):
         """
-            기존DB에 없던 앱을 추가할 때 호출됨.
+        (public)
+        기존DB에 없던 앱을 DB에 추가
         """
         try:
             self.cursor.execute("INSERT INTO list VALUES (?,?,?,?,?,?,?)",\
-                (new_app_name,package,imgSrc,updateDate,ratings,isDownloaded,category))
+                (new_app_name, package, img_src, update_date, ratings,\
+                    is_downloaded, category))
             logging.info(str(datetime.datetime.now()) + \
                 " - new record " + new_app_name + "is inserted in DB.")
         except Exception as e:
@@ -165,22 +175,15 @@ class DBController:
         self.connection.commit()
 
     def not_updated_list(self):
+        """
+        (public)
+        APK파일이 다운로드 되지 않은 파일의 리스트를 반환
+        """
         try:
             self.cursor.execute(\
-                "select package from list where isDownloaded LIKE ?",('%0%',))
+                "select package from list where is_downloaded LIKE ?",('%0%',))
             package_list = self.cursor.fetchall()
         except Exception as e:
             self.connection.close()
             raise e
         return package_list
-
-    def update_isdownload(self, package, isdownload):
-        if(isdownload):
-            self.cursor.execute(\
-                "update list set isDownloaded=? where package==?",\
-                ('True',package))
-        else:
-            self.cursor.execute(\
-                "update list set isDownloaded=? where package==?",\
-                ('False',package))
-        self.connection.commit()
